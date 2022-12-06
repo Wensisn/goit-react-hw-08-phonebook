3fee66d7542c50b42f065c66b86367f3e4b30087
Zubenko1 <zubenkodp@gmail.com>
2022-12-06 18:22:53 +0200

    .

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -8,14 +8,3 @@ const authSelectors = {
 };
 
 export default authSelectors;
-
-// export const selectVisibleTasks = createSelector(
-//   [selectContacts, selectFilter],
-//   (contacts, filter) => {
-//     const normolizedFilter = filter.toLowerCase();
-
-//     return contacts.items.filter(item =>
-//       item.name.toLowerCase().includes(normolizedFilter)
-//     );
-//   }
-// );
